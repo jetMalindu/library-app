a7c5eaa003bcaf051b0706bb5d97df01676e1ea0
jetMalindu <malidu334@gmail.com>
2023-08-04 23:01:13 +0530

    fix: Unnecessary component removed

--- library-app-client/src/App.tsx
+++ library-app-client/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BookListPage from "./pages/BookListPage";
-import BookCreatePage from "./pages/BookCreatePage";
 import BookDetailPage from "./pages/BookDetailPage";
-import AuthorCreatePage from "./pages/AuthorCreatePage";
 import { Layout } from "antd";
 import './App.css'
 const { Content } = Layout;
@@ -12,9 +10,7 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<BookListPage />} />
-          {/* <Route path="/books/:bookId" element={<BookDetailPage />} />
-      <Route path="/create-author" element={<AuthorCreatePage />} />
-      <Route path="/create-book" element={<BookCreatePage />} /> */}
+          <Route path="/books/:bookId" element={<BookDetailPage />} />
         </Routes>
       </Router>
     </div>
